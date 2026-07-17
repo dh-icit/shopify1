82514aa7e454008ca3e73023a4a3aa734676f018
Igor [de_ov] <igor.kolpin@gmail.com>
2026-07-17 15:22:55 +0300

    fix error js

--- theme/assets/custom.js
+++ theme/assets/custom.js
@@ -1,7 +1,11 @@
-const button = document.querySelector('.wt-cart__cross-sell__toggle');
+document.addEventListener("DOMContentLoaded", () => {
 
-button.addEventListener('click', () => {
-  const isExpanded = button.getAttribute('aria-expanded') === 'true';
-  button.closest('.wt-cart__cross-sell').classList.toggle('wt-cart__cross-sell--collapsed')
-  button.setAttribute('aria-expanded', !isExpanded);
+    const button = document.querySelector('.wt-cart__cross-sell__toggle');
+    if(button) {
+        button.addEventListener('click', () => {
+        const isExpanded = button.getAttribute('aria-expanded') === 'true';
+            button.closest('.wt-cart__cross-sell').classList.toggle('wt-cart__cross-sell--collapsed')
+            button.setAttribute('aria-expanded', !isExpanded);
+        });
+    }
 });
